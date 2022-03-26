9eaa6faa5c14ef87faed7754029f9bb13d59a918
Georgesui <gdimov445@gmail.com>
2022-03-26 22:21:23 +0200

    till the end

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 console.log('------------- # 4.a')
 function newCalc(a, b) {
-	return a + b
+	return a + b;
 }
 console.log(newCalc(3, 4))
 console.log('------------- # 4.b')
 function secondCalc(a, b) {
-	return a - b
+	return a - b;
 }
 console.log(secondCalc(3, 4))
 console.log('------------- # 4.c')
@@ -22,9 +22,9 @@ console.log('------------- # 5')
 function countNumbers(n) {
 	let newArray = [];
 	for (i = 1; i <= n; i++) {
-		newArray.push(i)
+		newArray.push(i);
 	}
-	return newArray
+	return newArray;
 }
 console.log(countNumbers(5))
 console.log('------------- # 6')
@@ -34,15 +34,33 @@ function reduceNumbers(n) {
 	while (newCounter > 0) {
 		secondArray.push(newCounter--);
 	}
-	return secondArray
+	return secondArray;
 }
 console.log(reduceNumbers(5))
 console.log('------------- # 7')
 function pow(x, n) {
 	let firstCount = x;
 	for (i = 1; i < n; i++) {
-		firstCount *= x
+		firstCount *= x;
 	}
-	return firstCount
+	return firstCount;
 }
 console.log(pow(3, 4))
+console.log('------------- # 8')
+function isBigger(a, b) {
+	if (a > b) {
+		return true;
+	} else {
+		return false;
+	}
+}
+console.log(isBigger(5, -1));
+console.log('------------- # 9')
+function isSmaller(a, b) {
+	if (a < b) {
+		return true;
+	} else {
+		return false;
+	}
+}
+console.log(isSmaller(5, -1));
